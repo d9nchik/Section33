bec3c91d16fcef31a0b0e7efeda680478ffb118b
d9nich <dan.galaiko@gmail.com>
2020-07-20 20:15:41 +0300

    Part of exercise9 has been done.

---
+++ src/sample/exercise9/ChatClientController.java
@@ -0,0 +1,15 @@
+package sample.exercise9;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+
+public class ChatClientController {
+    @FXML
+    private TextArea clientText, serverText;
+
+    @FXML
+    public void submit(){
+
+    }
+}
